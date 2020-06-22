5995d0085207c10c628f7d7f8ff6012b1b11ca56
peterow <32960273+peterow@users.noreply.github.com>
2020-06-22 14:46:48 +0100

    Test 'OneDimeInserted_Display_ShowsCorrectAmount' passing.

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Globalization;
 
 namespace VendingMachineKata
 {
     public class VendingMachine
     {
-        public string Display { get { return "INSERT COIN"; } }
+        private decimal _amountInDollars = 0;
+        
+        public string Display { get { return _amountInDollars > 0 ? string.Format("${0:0.00}", _amountInDollars) :"INSERT COIN"; } }
 
-        public void InsertCoin(CoinType dime)
+        public void InsertCoin(CoinType coinType)
         {
-            throw new NotImplementedException();
+            _amountInDollars += 0.1M;
         }
     }
 }
